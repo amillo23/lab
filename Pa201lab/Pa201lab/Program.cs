bfb166a6dc67057946cc38bbd01533755e565e80
amillo23 <amil.maharramli@bk.ru>
2025-09-05 17:35:38 +0400

    completed tasks

---
+++ Pa201lab/Pa201lab/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Drawing;
+string input = Console.ReadLine();
+int number =Convert.ToInt32(input);
+if (number >= 90 && number <= 100)
+{
+    Console.WriteLine("A");
+}
+
+else if (number >= 70 && number <= 89)
+{
+    Console.WriteLine("B");
+}
+else if (number >= 50 && number <= 69)
+{
+
+    Console.WriteLine("C");
+}
+else if (number < 50 && number>0)
+{
+
+    Console.WriteLine("F");
+}
+else
+{
+    Console.WriteLine("Invalid");
+}
